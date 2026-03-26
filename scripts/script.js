cdb5a79e0d9063228b9291d1525ef21c64379385
TheTeaOne <125920852+TheTeaOne@users.noreply.github.com>
2026-03-26 16:22:31 +0100

    feat: add auto-expanding textarea logic and fix height reset

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,15 @@ const sidebar = document.querySelector(".sidebar")
 burger.addEventListener("click",function(){
     sidebar.classList.toggle("open")
 })
+
+const noteInput = document.querySelector('.note-input')
+    if(noteInput){
+    noteInput.addEventListener('input', function(){
+        this.style.height = 'auto'
+        if(this.vaule === ''){
+            this.style.height = ''
+        }else{
+            this.style.height = this.scrollHeight + 'px'
+        }
+    })
+}
