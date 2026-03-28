2f876cf2353d472d67776dbcd2d2b4f218920db5
TheTeaOne <125920852+TheTeaOne@users.noreply.github.com>
2026-03-28 18:03:40 +0100

    feat: add note expansion logic and auto-resizing textarea

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,31 @@
 const burger = document.getElementById("burger")
 const sidebar = document.querySelector(".sidebar")
 
-burger.addEventListener("click",function(){
+const noteContainer = document.getElementById('noteContainer')
+const noteInput = document.querySelector('.note-input')
+const titleBlock = document.getElementById('titleBlock')
+
+burger.addEventListener("click", function() {
     sidebar.classList.toggle("open")
 })
 
-const noteInput = document.querySelector('.note-input')
-    if(noteInput){
-    noteInput.addEventListener('input', function(){
-        this.style.height = 'auto'
-        if(this.vaule === ''){
-            this.style.height = ''
-        }else{
-            this.style.height = this.scrollHeight + 'px'
-        }
+if (noteInput) {
+    noteInput.addEventListener('input', function() {
+        this.style.height = 'auto';
+        this.style.height = this.scrollHeight + 'px'
+    })
+
+    noteInput.addEventListener('focus', function() {
+        noteContainer.classList.add('active')
+        titleBlock.classList.remove('hidden')
     })
 }
+
+document.addEventListener('click', function(event) {
+    if (noteContainer && !noteContainer.contains(event.target) && noteInput.value.trim() === "") {
+        noteInput.value = ""
+        noteContainer.classList.remove('active')
+        titleBlock.classList.add('hidden')
+        noteInput.style.height = 'auto'
+    }
+})
