8ca7316af448bc3ba6fd2eaf314d62913e8d895b
TheTeaOne <125920852+TheTeaOne@users.noreply.github.com>
2026-04-07 14:00:23 +0200

    feat: collect note data and log to console

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,8 @@ const sidebar = document.querySelector(".sidebar")
 const noteContainer = document.getElementById('noteContainer')
 const noteInput = document.querySelector('.note-input')
 const titleBlock = document.getElementById('titleBlock')
+const createBtn = document.getElementById('createBlock')
+const titleInput = document.querySelector('.title-input')
 
 burger.addEventListener("click", function() {
     sidebar.classList.toggle("open")
@@ -18,14 +20,33 @@ if (noteInput) {
     noteInput.addEventListener('focus', function() {
         noteContainer.classList.add('active')
         titleBlock.classList.remove('hidden')
+        createBtn.classList.remove('hidden')
     })
 }
 
 document.addEventListener('click', function(event) {
     if (noteContainer && !noteContainer.contains(event.target) && noteInput.value.trim() === "") {
         noteInput.value = ""
+        if (noteInput) titleInput.value = ''
         noteContainer.classList.remove('active')
         titleBlock.classList.add('hidden')
+        createBtn.classList.add('hidden')
         noteInput.style.height = 'auto'
     }
 })
+
+if (createBtn){
+    createBtn.addEventListener('click', function(){
+        const noteDate = {
+            title: titleInput ? titleInput.value: '',
+            text: noteInput.value
+        }
+        console.log("Note was Created", noteDate)
+        titleInput.value = ''
+        noteInput.value = ''
+        noteContainer.classList.remove('active')
+        titleBlock.classList.add('hidden')
+        createBtn.classList.add('hidden')
+        noteInput.style.height = 'auto'
+    })
+}
