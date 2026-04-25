957cf6f3e007ef3865f3334ffe040fa97977d50c
TheTeaOne <125920852+TheTeaOne@users.noreply.github.com>
2026-04-25 14:54:05 +0200

    feat: implement Recycle Bin with SPA-style state switching and note restoration

--- scripts/script.js
+++ scripts/script.js
@@ -19,7 +19,8 @@ const saveBtn = document.getElementById('saveModal')
 let currentEditingId = null
 
 let notes = JSON.parse(localStorage.getItem('notes')) || []
-
+let trash = JSON.parse(localStorage.getItem('trash')) || []
+let isTrashMode = false
 burger.addEventListener("click", function() {
     sidebar.classList.toggle("open")
 })
@@ -71,6 +72,11 @@ function checkEmpty(){
     }
 }
 
+document.getElementById('openTrash').addEventListener('click', function(){
+    isTrashMode = !isTrashMode
+    refreshNotes()
+})
+
 document.addEventListener('click', function(event) {
     if (noteContainer && !noteContainer.contains(event.target) && noteInput.value.trim() === "") {
         noteInput.value = ""
@@ -84,6 +90,7 @@ document.addEventListener('click', function(event) {
 
 function saveNotes() {
     localStorage.setItem('notes', JSON.stringify(notes))
+    localStorage.setItem('trash', JSON.stringify(trash))
 }
 function addNoteToArray (title, text){
         const newNote = {
@@ -155,18 +162,28 @@ function refreshNotes(){
     const notesWrapper = document.getElementById('notesWrapper')
     const pinnedSection = document.getElementById('pinnedSection')
     const othersTitle = document.getElementById('othersTitle')
-    pinnedWrapper.innerHTML = ""
-    notesWrapper.innerHTML = ''
+    const noteContainer = document.getElementById('noteContainer')
+        pinnedWrapper.innerHTML = ""
+        notesWrapper.innerHTML = ''
+    if(!isTrashMode){
+        noteContainer.classList.remove('hidden')
+
     const pinnedNotes = notes.filter(n => n.isPinned)
     const otherNotes = notes.filter(n => !n.isPinned)
-    pinnedNotes.forEach(note => renderNote(note, pinnedWrapper))
-    otherNotes.forEach(note => renderNote(note, notesWrapper))
+        pinnedNotes.forEach(note => renderNote(note, pinnedWrapper))
+        otherNotes.forEach(note => renderNote(note, notesWrapper))
     if(pinnedNotes.length > 0){
         pinnedSection.classList.remove('hidden')
         othersTitle.classList.remove('hidden')
     } else{
         pinnedSection.classList.add('hidden')
         othersTitle.classList.add('hidden')
+    }
+}   else{
+        noteContainer.classList.add('hidden')
+        pinnedSection.classList.add('hidden')
+        othersTitle.classList.add('hidden')
+        trash.forEach(note => renderNote(note, notesWrapper))
     }
     checkEmpty()
 }
@@ -192,6 +209,17 @@ function renderNote(note, container){
         deleteBtn.innerHTML = `<svg width="20" height="20" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M1 6.33333H25M10 11.6667V19.6667M16 11.6667V19.6667M2.5 6.33333L4 22.3333C4 23.0406 4.31607 23.7189 4.87868 24.219C5.44129 24.719 6.20435 25 7 25H19C19.7956 25 20.5587 24.719 21.1213 24.219C21.6839 23.7189 22 23.0406 22 22.3333L23.5 6.33333M8.5 6.33333V2.33333C8.5 1.97971 8.65804 1.64057 8.93934 1.39052C9.22064 1.14048 9.60218 1 10 1H16C16.3978 1 16.7794 1.14048 17.0607 1.39052C17.342 1.64057 17.5 1.97971 17.5 2.33333V6.33333" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>`
+        if (note.isPinned) pinBtn.classList.add('active')
+            if (!isTrashMode) {
+        card.appendChild(pinBtn)
+    }
+
+    const restoreBtn = document.createElement('div');
+    restoreBtn.classList.add('restoreBtn');
+    restoreBtn.innerHTML = `<svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M3 10H13C17.4183 10 21 13.5817 21 18V20M3 10L8 5M3 10L8 15" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>` 
+        
     pinBtn.addEventListener('click', function(e){
             e.stopPropagation()
             note.isPinned = !note.isPinned
@@ -200,12 +228,27 @@ function renderNote(note, container){
     })
     deleteBtn.addEventListener('click',function(event){
         event.stopPropagation()
-        notes = notes.filter(note => note.id !== id)
+    if(!isTrashMode){
+        const noteToTrash = notes.find(n  => n.id === id)
+        trash.push(noteToTrash)
+        notes = notes.filter(n => n.id !== id)
+    } else{
+        trash = trash.filter(n => n.id !== id)
+    }
         saveNotes()
         card.remove()
-        checkEmpty()
+        refreshNotes()
     })
-        card.appendChild(pinBtn)
+        restoreBtn.addEventListener('click', function(event) {
+        event.stopPropagation()
+        const noteToRestore = trash.find(n => n.id === id)
+        if (noteToRestore) {
+            notes.push(noteToRestore)
+            trash = trash.filter(n => n.id !== id)
+            saveNotes()
+            refreshNotes()
+        }
+    })
 if(title){
     const h3 = document.createElement('h3')
     h3.innerText = title
@@ -217,5 +260,8 @@ const p = document.createElement('p')
     footer.appendChild(deleteBtn)
     card.appendChild(footer)
     container.prepend(card)
+if (isTrashMode) {
+        footer.appendChild(restoreBtn)
+    }
 }
     refreshNotes()
